6e5ad85f8f7a0dec3c75d2a7106c1908cef49ca1
Marcos Fellipe Guimarães <marcosfgui2006@gmail.com>
2023-05-16 22:57:05 -0300

    Feature:
    Verificação dos breakpoints de tela e redirecionamento confome o tamanho da tela, podendo ser para a Web, tablets ou mobile.

---
+++ lib/View/Tablets/tablets.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Tablets extends StatefulWidget {
+  const Tablets({super.key});
+
+  @override
+  State<Tablets> createState() => _TabletsState();
+}
+
+class _TabletsState extends State<Tablets> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('Tablets')),
+    );
+  }
+}
