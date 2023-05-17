8476031ecb56046d3f3e5703648fa8c22cbd61ef
Marcos Fellipe Guimarães <marcosfgui2006@gmail.com>
2023-05-16 22:13:27 -0300

    Initial commit

---
+++ android/app/src/main/java/com/example/esteticaemovimento/app_esteticaemovimento/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.esteticaemovimento.app_esteticaemovimento;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+import 'View/Home.dart';
+
+void main(){
+
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+
+}
